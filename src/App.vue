a8873c22dfb203e338e9a79fa1d8e47b72d0a7ca
railombardi <raidslombardi@gmail.com>
2023-03-28 19:37:20 -0300

    feat(navbar): :sparkles: create initial navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav
+      class="relative flex flex-wrap items-center justify-between px-2 py-3 mb-3"
+    >
+      <mdicon name="menu" />
+      <a
+        class="text-sm font-medium leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+      >
+        FakeStore
+      </a>
+      <mdicon name="cart-outline" />
     </nav>
     <router-view />
   </div>
 </template>
+<script>
+export default {};
+</script>
+<style>
+#app {
+  font-family: "Clash Display", sans-serif;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+import mdiVue from "mdi-vue/v2";
+import * as mdijs from "@mdi/js";
 
 Vue.config.productionTip = false;
+
+Vue.use(mdiVue, {
+  icons: mdijs,
+});
 
 new Vue({
   router,
